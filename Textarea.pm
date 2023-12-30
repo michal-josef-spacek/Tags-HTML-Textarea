4965175a217f6cb32df14166952dedc99099bad6
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-12-30 20:21:04 +0100

    Prepare for 0.02 version

--- Textarea.pm
+++ Textarea.pm
@@ -8,7 +8,7 @@ use Class::Utils qw(set_params split_params);
 use Error::Pure qw(err);
 use Scalar::Util qw(blessed);
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 # Process 'Tags'.
 sub _process {
@@ -261,6 +261,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.01
+0.02
 
 =cut
